8a6793955f4882e121b81cc3ca078ad1abdb6c75
Ventura Del Monte <venturadelmonte@gmail.com>
2020-02-27 17:38:32 +0100

    Add bootstrap and reactstrap

---
+++ client/src/routes/home/Form.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {Button, Input} from "reactstrap";
+
+export default class Form extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  change = e => {
+    this.props.onChange({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    // this.props.onSubmit(this.state);
+    this.setState({
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: ""
+    });
+    this.props.onChange({
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: ""
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <Input
+          name="firstName"
+          placeholder="First name"
+          value={this.state.firstName}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <Input
+          name="lastName"
+          placeholder="Last name"
+          value={this.state.lastName}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <Input
+          name="username"
+          placeholder="Username"
+          value={this.state.username}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <Input
+          name="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <Button onClick={e => this.onSubmit(e)}>Submit</Button>
+      </form>
+    );
+  }
+}
--- frontend/src/routes/home/home.js
+++ frontend/src/routes/home/home.js
@@ -1,88 +1,24 @@
 import React from 'react'
-import TextInput from './TextInput';
-import TextArea from './TextArea';
-import Email from './Email';
+import Form from "./Form";
 class Home extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            formIsValid: false,
-            formControls: {
-
-              name: {
-                value: '',
-                placeholder: 'What is your name',
-                valid: false,
-                validationRules: {
-                  minLength: 4,
-                  isRequired: true
-                },
-                touched: false
-              },
-              address: {
-                value: '',
-                placeholder: 'What is your address',
-                valid: false,
-                validationRules: {
-                  minLength: 4,
-                  isRequired: true
-                },
-                touched: false
-              },
-              email: {
-                value: '',
-                placeholder: 'What is your email',
-                valid: false,
-                validationRules: {
-                  isRequired: true,
-                  isEmail: true
-                },
-                touched: false
-              },
-            }
+          fields: {}
         }
 
-
     }
 
-    changeHandler = event => {
-
-      const name = event.target.name;
-      const value = event.target.value;
-
-      const updatedControls = {
-        ...this.state.formControls
-      };
-      const updatedFormElement = {
-        ...updatedControls[name]
-      };
-      updatedFormElement.value = value;
-      updatedFormElement.touched = true;
-      updatedFormElement.valid = validate(value, updatedFormElement.validationRules);
-
-      updatedControls[name] = updatedFormElement;
-
-      let formIsValid = true;
-      for (let inputIdentifier in updatedControls) {
-        formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
-      }
-
+    onChange = updatedValue => {
       this.setState({
-        formControls: updatedControls,
-        formIsValid: formIsValid
+        fields: {
+          ...this.state.fields,
+          ...updatedValue
+        }
       });
+    };
 
-    }
-
-    formSubmitHandler = () => {
-      const formData = {};
-      for (let formElementId in this.state.formControls) {
-        formData[formElementId] = this.state.formControls[formElementId].value;
-      }
-
-      console.dir(formData);
-    }
 
     // componentDidMount() {
     //     fetch("/hello")
@@ -106,35 +42,11 @@ class Home extends React.Component {
     render() {
         return (
           <div>
-          <TextInput name="name"
-            placeholder={this.state.formControls.name.placeholder}
-            value={this.state.formControls.name.value}
-            onChange={this.changeHandler}
-            touched={this.state.formControls.name.touched}
-            valid={this.state.formControls.name.valid}
-            />
-
-            <TextArea name="address"
-            placeholder={this.state.formControls.address.placeholder}
-            value={this.state.formControls.address.value}
-            onChange={this.changeHandler}
-            touched={this.state.formControls.address.touched}
-            valid={this.state.formControls.address.valid}
-            />
-
-            <Email name="my_email"
-            placeholder={this.state.formControls.my_email.placeholder}
-            value={this.state.formControls.my_email.value}
-            onChange={this.changeHandler}
-            touched={this.state.formControls.my_email.touched}
-            valid={this.state.formControls.my_email.valid}
-            />
-            <button onClick={this.formSubmitHandler}
-                disabled={! this.state.formIsValid}
-            >
-                Submit
-                </button>
-           </div>
+            <Form onChange={fields => this.onChange(fields)} />
+            <p>
+              {JSON.stringify(this.state.fields, null, 2)}
+            </p>
+          </div>
         )
     }
 }
